d53898ad08f4979510d3ffc97b7a709cb6179664
Luigi Rigato <60691765+LuigiRigato@users.noreply.github.com>
2022-05-12 10:53:28 -0300

    Add files via upload
    Uploading as classes ainda não feitas

---
+++ lab05/src/pt/c40task/l05wumpus/App.java
@@ -0,0 +1,7 @@
+package pt.c40task.l05wumpus;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
---
+++ lab05/src/pt/c40task/l05wumpus/Caverna.java
@@ -0,0 +1,13 @@
+package pt.c40task.l05wumpus;
+
+public class Caverna
+{
+    private Salas[][] tabuleiro = new Salas[4][4];
+
+    public void setTabuleiroMontador()
+    {
+        
+    }
+
+
+}
