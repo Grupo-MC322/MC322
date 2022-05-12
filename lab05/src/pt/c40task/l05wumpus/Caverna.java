8990481f8852b38a302f7e56153fdff2b88fb868
Raphael Ferezin Kitahara <raphael@MacBook-Air-de-Raphael.local>
2022-05-12 15:20:28 -0300

    atualiza

--- lab05/src/pt/c40task/l05wumpus/Caverna.java
+++ lab05/src/pt/c40task/l05wumpus/Caverna.java
@@ -4,9 +4,9 @@ public class Caverna
 {
     private Salas[][] tabuleiro = new Salas[4][4];
 
-    public void setTabuleiroMontador()
+    public void setTabuleiroMontador(int linha, int coluna, Componentes componente)
     {
-        
+        Salas[linha][coluna] = componente;
     }
 
 
