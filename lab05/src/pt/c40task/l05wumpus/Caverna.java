5dfdde33471daf9f7caee80a0745ce824c6fd8c9
LuigiRigato <l247248@dac.unicamp.br>
2022-05-12 15:39:12 -0300

    update pastas concluido

---
+++ lab05/src/pt/c40task/l05wumpus/Caverna.java
@@ -0,0 +1,17 @@
+package pt.c40task.l05wumpus;
+
+public class Caverna
+{
+    private Salas[][] tabuleiro = new Salas[4][4];
+
+    public void setTabuleiroMontador()
+    {
+        for (int linha = 0; linha < 4; linha++)
+        {
+            for (int coluna = 0; )
+        }
+    }
+
+
+}
+
