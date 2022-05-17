02f33a0c9c51ac0ac372f34fb1ac9c892a791e58
Raphael Ferezin Kitahara <raphael@MacBook-Air-de-Raphael.local>
2022-05-17 14:30:53 -0300

    adiciona funções da flecha e desenvolvimento do controle

--- lab05/src/pt/c40task/l05wumpus/Controle.java
+++ lab05/src/pt/c40task/l05wumpus/Controle.java
@@ -8,6 +8,7 @@ public class Controle
     private int posicaoX, posicaoY;
     private boolean achouOuro = false;
     private String status;
+    private Heroi heroi;
 
     public void movimentosArquivo(Caverna caverna, String movimentos)
     {
@@ -16,6 +17,8 @@ public class Controle
         for(int i = 0; i < movimentos.length(); i++)
         {
             char movimentoAtual = movimentos.charAt(i);
+
+        
             if(movimentoAtual == 'w' && posicaoY-1 >= 0)
             {
                 movimentar(posicaoX, posicaoY, posicaoX, posicaoY-1, caverna);
@@ -37,41 +40,100 @@ public class Controle
                 posicaoY = posicaoX+1;
             }
 
+            // definições da flecha
+            if(heroi.getFlechaEquipada()) // se a flecha estiver equipada, significa que ela não foi utilizada e foi perdida
+            {
+                heroi.setFlechaEquipada(false);
+            }
+
+            if(movimentoAtual == 'k')
+            {
+                if(heroi.getFlechaUsada()) // se a flecha já tiver sido utilizada
+                {
+                    System.out.println("A flecha já foi utilizada.");
+                }
+                else // equipa a flecha e muda para como sendo utilizada
+                {
+                    heroi.setFlechaEquipada(true);
+                    heroi.setFlechaUsada(true);
+                    pontuacao -= 100;
+                }
+            }
+
             caverna.setTabuleiro(posicaoX, posicaoY-1, caverna.getComponente(posicaoX, posicaoY));
 
-            // write board
+            // write board FAZER
         }
+    }
+
+    public void conectaHeroi(Heroi heroi)
+    {
+        this.heroi = heroi;
     }
 
     public void movimentar(int xInicio, int yInicio, int xFim, int yFim, Caverna caverna)
     {
-        if(caverna.getInfo(xFim, yFim) == 'W') // se for Wumpus
+        switch(caverna.getInfo(xFim, yFim))
         {
-            if(ganharBatalha())
-            {
-                pontuacao += 500;
-                caverna.setTabuleiro(xFim, yFim, caverna.getComponente(xInicio, yInicio));
-            } else {
+            case 'W':
+                if(heroi.getFlechaEquipada()) // se a flecha estiver equipada
+                {
+                    if(ganharBatalha()) // se o herói ganhar a batalha
+                    {
+                        pontuacao += 500;
+                        caverna.setTabuleiro(xFim, yFim, caverna.getComponente(xInicio, yInicio));
+                        heroi.setFlechaEquipada(false); 
+                    } 
+                    else // se perder a batalha
+                    {
+                        pontuacao -= 1000;
+                        // perdeu jogo FAZER
+                    }
+                } 
+                else 
+                {
+                    pontuacao -= 1000;
+                    // perdeu jogo FAZER
+                }
+                break;
+
+            case 'B':
                 pontuacao -= 1000;
-                // perdeu jogo
-            }
-        } else if(caverna.getInfo(xFim, yFim) == 'B')
-        {
-            pontuacao += -1000;
-            // perdeu jogo
-        } else if(caverna.getInfo(xFim, yFim) == 'O')
-        {
-            achouOuro = true;
-        } else if(caverna.getInfo(xFim, yFim) == 'b'){
-            status = "Brisa";
-        } else if(caverna.getInfo(xFim, yFim) == 'f'){
-            status = "Fedor";
-        } else if(caverna.getInfo(xFim, yFim) == '-'){
-        
+                // perdeu jogo FAZER
+                break;
+
+            case 'O':
+                achouOuro = true;
+                break;
+
+            case 'b':
+                status = "Brisa";
+                break;
+            
+            case 'f':
+                status = "Fedor";
+                break;
+            
+            case '-':
+                break;
+
+            default:
+                break;
         }
     }
+    
+    public boolean ganharBatalha()
+    {
+        Random rand = new Random();
+        int resultado = rand.nextInt(1);
+        if(resultado == 0){ // se o jogador perder
+            return false;
+        }
+        return true; // se o jogador ganhar
+    }
 
-    public int getPontuacao(){
+    public int getPontuacao()
+    {
         return pontuacao;
     }
 
@@ -83,15 +145,5 @@ public class Controle
     public void ganhou()
     {
 
-    }
-
-    public boolean ganharBatalha()
-    {
-        Random rand = new Random();
-        int resultado = rand.nextInt(1);
-        if(resultado == 0){ // se o jogador perder
-            return false;
-        }
-        return true; // se o jogador ganhar
     }
 }
--- lab05/src/pt/c40task/l05wumpus/Heroi.java
+++ lab05/src/pt/c40task/l05wumpus/Heroi.java
@@ -3,8 +3,25 @@ package pt.c40task.l05wumpus;
 public class Heroi 
 {
     private char info = 'P';
-    private int flechas;
-    private boolean flechaEquipada;
+    private boolean flechaUsada = false;
+    private boolean flechaEquipada = false;
 
+    
+
+    public boolean getFlechaUsada(){
+        return flechaUsada;
+    }
+
+    public void setFlechaUsada(boolean flechaUsada) {
+        this.flechaUsada = flechaUsada;
+    }
+
+    public boolean getFlechaEquipada(){
+        return flechaEquipada;
+    }
+
+    public void setFlechaEquipada(boolean flechaEquipada) {
+        this.flechaEquipada = flechaEquipada;
+    }
 
 }
