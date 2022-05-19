4b72d1f439a322a1c1fe6cca10865f800dd1d807
LuigiRigato <l247248@dac.unicamp.br>
2022-05-19 11:37:22 -0300

    teclado manual

--- lab05/src/pt/c40task/l05wumpus/AppWumpus.java
+++ lab05/src/pt/c40task/l05wumpus/AppWumpus.java
@@ -1,5 +1,7 @@
 package pt.c40task.l05wumpus;
 
+import java.util.Scanner;
+
 public class AppWumpus {
 
    public static void main(String[] args) {
@@ -10,9 +12,10 @@ public class AppWumpus {
    }
    
    public static void executaJogo(String arquivoCaverna, String arquivoSaida,
-                                  String arquivoMovimentos) {
+                                  String arquivoMovimentos)
+   {
       Toolkit tk = Toolkit.start(arquivoCaverna, arquivoSaida, arquivoMovimentos);
-      
+
       Heroi heroiJogo = new Heroi();
       Caverna cavernaJogo = new Caverna();
       new Montador(tk, cavernaJogo, heroiJogo);
@@ -21,14 +24,28 @@ public class AppWumpus {
       heroiJogo.conectaControle(controleJogo);
       controleJogo.conectaHeroi(heroiJogo);
 
+      if(arquivoMovimentos == null)
+      {
+         
+      }
+
+      Scanner teclado = new Scanner(System.in);
+      System.out.println();
+      String nome = teclado.nextLine();
+      teclado.close();
+      
       String movements = tk.retrieveMovements();
       System.out.println("=== Movimentos");
       System.out.println(movements);
       
+      if(movements.length() == 0)
+      {
+
+      }
       for(int i = 0; i < movements.length(); i++)
       {
          controleJogo.movimentosArquivo(cavernaJogo, movements, i);
-         if(controleJogo.getStatus() == 'n') // se o jogador perder
+         if(controleJogo.getStatus() == 'L') // se o jogador perder
          {
             tk.writeBoard(cavernaJogo.apresentar(), controleJogo.getPontuacao(), controleJogo.getStatus());
             break;
@@ -39,8 +56,27 @@ public class AppWumpus {
          }
       }
 
+      char[][] board = cavernaJogo.apresentar();
       System.out.println("=== Última Caverna");
-      tk.writeBoard(cavernaJogo.apresentar(), controleJogo.getPontuacao(), controleJogo.getStatus());
+      for(int i = 0; i < 4; i++)
+      {
+         for(int j = 0; j < 4; j++)
+         {
+            System.out.println(board[i][j]);
+         }
+         System.out.println();
+      }
+      System.out.println("Player: " + nome);
+      System.out.println("Score: " + controleJogo.getPontuacao());
+      switch (controleJogo.getStatus()) {
+         case 'W':
+            System.out.println("Voce ganhou =D !!!");
+            break;
+         case 'L':
+            System.out.println("Voce perdeu =( !!!");
+            break;
+      }
+      tk.writeBoard(board, controleJogo.getPontuacao(), controleJogo.getStatus());
 
       
       tk.stop();
@@ -51,7 +87,6 @@ public class AppWumpus {
 /*     -  Listinha de tarefas  -    */
 
 // fazer modos automático e com teclado.
-// cada user tem nome? tem que alterar o toolkit
 // quem aciona a leitura do arquivo é o montador
 // status como W, P, L ou n, x, w
 // como deve sair
