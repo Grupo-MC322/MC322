9eeae0b5c94aec26ec0deb24a43538c044c88596
RaphaelFK <raphael_kitahara@hotmail.com>
2022-05-19 13:50:12 -0300

    Adiciona comentários

--- lab05/src/pt/c40task/l05wumpus/Caverna.java
+++ lab05/src/pt/c40task/l05wumpus/Caverna.java
@@ -7,6 +7,7 @@ public class Caverna
 
     public Caverna()
     {
+        // setup inicial do tabuleiro com uma sala em cada posição
         for(int i = 0; i < 4; i++)
         {
             for(int j = 0; j < 4; j++)
@@ -16,42 +17,46 @@ public class Caverna
         }
     }
 
-    public void addComponente(int linha, int coluna, Componentes componente)
-    {
-        tabuleiro[linha][coluna].addComponente(componente);
-    }
-
-    public void delComponente(int linha, int coluna, Componentes componente)
-    {
-        tabuleiro[linha][coluna].delComponente(componente);
-    }
-
+    // recebe o componente predominante da sala posicionada na posição indicada
     public Componentes getComponente(int linha, int coluna, char info)
     {
         return tabuleiro[linha][coluna].getComponente(info);
     }
 
+    // adiciona um componente recebido na linha e na coluna indicadas do tabuleiro
+    public void addComponente(int linha, int coluna, Componentes componente)
+    {
+        tabuleiro[linha][coluna].addComponente(componente);
+    }
+
+    // remove um componente recebido da linha e da coluna indicadas do tabuleiro
+    public void delComponente(int linha, int coluna, Componentes componente)
+    {
+        tabuleiro[linha][coluna].delComponente(componente);
+    }
+
+    // retorna uma matriz de char com as informações do componente predominante de cada sala
     public char[][] apresentar()
     {
         for(int i = 0; i < 4; i++)
         {
             for(int j = 0; j < 4; j++)
             {
-                if(tabuleiro[i][j].getComponente('-') != null)
+                if(tabuleiro[i][j].getComponente('-') != null) // se a sala ainda não tiver sido descoberta
                     cave[i][j] = '-';
-                else if(tabuleiro[i][j].getComponente('W') != null)
+                else if(tabuleiro[i][j].getComponente('W') != null) // se tiver Wumpus na sala
                     cave[i][j] = 'W';
-                else if(tabuleiro[i][j].getComponente('O') != null)
+                else if(tabuleiro[i][j].getComponente('O') != null) // se tiver Ouro na sala
                     cave[i][j] = 'O';
-                else if(tabuleiro[i][j].getComponente('B') != null)
+                else if(tabuleiro[i][j].getComponente('B') != null) // se tiver Buraco na sala
                         cave[i][j] = 'B';
-                else if(tabuleiro[i][j].getComponente('P') != null)
+                else if(tabuleiro[i][j].getComponente('P') != null) // se o Herói estiver na sala
                     cave[i][j] = 'P';
-                else if(tabuleiro[i][j].getComponente('f') != null)
+                else if(tabuleiro[i][j].getComponente('f') != null) // se tiver fedor na sala
                     cave[i][j] = 'f';
-                else if(tabuleiro[i][j].getComponente('b') != null)
+                else if(tabuleiro[i][j].getComponente('b') != null) // se tiver brisa na sala
                     cave[i][j] = 'b';
-                else
+                else // se a sala for vazia e já tiver sido visitada
                     cave[i][j] = '#';
             }
         }
--- lab05/src/pt/c40task/l05wumpus/Montador.java
+++ lab05/src/pt/c40task/l05wumpus/Montador.java
@@ -7,8 +7,9 @@ public class Montador
 
     public Montador(Toolkit tk, Caverna caverna, Heroi heroi)
     {
+        // receber e imprimir as instruções para a montagem da caverna
         String instrucoes[][] = tk.retrieveCave();
-        System.out.println("=== Caverna");
+        System.out.println("=== Caverna"); 
         for(int l = 0; l < instrucoes.length; l++)
         {
             for(int c = 0; c < instrucoes[l].length; c++)
@@ -16,16 +17,20 @@ public class Montador
             System.out.println();
         }
         
+        // setup de cada posição da caverna
         for(int i = 0; i < instrucoes.length; i++)
         {
             linha = Integer.parseInt(instrucoes[i][0])-1;
             coluna = Integer.parseInt(instrucoes[i][1])-1;
+
+            // condições para a montagem das posições (encerra o programa caso identifique algum erro)
             if(linha < 0 || linha > 3 || coluna < 0 || coluna > 3)
             {
                 System.out.println("Foi inserida uma posição fora do tabuleiro.");
                 System.exit(0);
             }
 
+            // definição de qual componente vai ser adicionado a cada sala da caverna
             switch (instrucoes[i][2])
             {
                 case "P":
@@ -46,13 +51,14 @@ public class Montador
                     break;
             }
             
-            // toda sala é iniciada com um espaço vazio que fica com a menor prioridade dentro dela
+            // um espaço vazio é adicionado em todas as salas, o qual fica com a menor prioridade dentro dela
             caverna.addComponente(linha, coluna, new Vazio());
             caverna.addComponente(linha, coluna, new Visitado());
         }
+        // deleta o componente "Vazio" da entrada da caverna, onde o herói começa
         caverna.delComponente(0, 0, caverna.getComponente(0, 0, '-'));
 
-        // o montador encerra o programa se identificar algum erro de montagem
+        // o montador encerra o programa se identificar algum erro de montagem com relação à quantidade de cada componente
         if(contaBuraco < 2 || contaBuraco > 3 || contaWumpus != 1 || contaOuro != 1 || contaHeroi != 1)
         {
             System.out.println("Erro na montagem do jogo, encerrando programa...");
